fa879f98b24bbf8de53c64a02511197a3dfb8893
Prasad Birajdar <prasad.birajdar@thinkitive.com>
2025-03-29 17:31:39 +0530

    Fixed unused variables issue

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,10 +3,8 @@ import { Container, Row, Col } from "react-bootstrap";
 import ProjectCard from "./ProjectCards";
 import Particle from "../Particle";
 import crystalImg from "../../Assets/Projects/crystal-img.png";
-import emotion from "../../Assets/Projects/emotion.png";
 import docsinkImg from "../../Assets/Projects/docsink.png";
 import fitAngle from "../../Assets/Projects/fit_angle.png";
-import suicide from "../../Assets/Projects/suicide.png";
 import ortholiveImg from "../../Assets/Projects/ortholiveImg.png";
 
 function Projects() {
